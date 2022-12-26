cad77fdef6679abb86e5f9aac2a45bffbee7db71
Pablo Deymonnaz <deymonnaz@gmail.com>
2022-12-26 19:17:11 -0300

    MarlinProof

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+use ark_bls12_377::{Bls12_377, Fr, FrParameters, Parameters};
+use ark_ec::bls12::Bls12;
+use ark_ff::Fp256;
+use ark_marlin::{IndexProverKey, IndexVerifierKey, Marlin, Proof, SimpleHashFiatShamirRng};
+use ark_poly::univariate::DensePolynomial;
+use ark_poly_commit::marlin_pc::MarlinKZG10;
+
 pub mod aes;
 pub mod ops;
 pub mod prover;
+
+pub type MarlinProof =
+    Proof<Fr, MarlinKZG10<Bls12<Parameters>, DensePolynomial<Fp256<FrParameters>>>>;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use poc_encryption_proof::ops;
+use poc_encryption_proof::{ops, MarlinProof};
 
 fn main() {
 }
 
-fn encrypt_block_128(plain_text: u128, key: u128) -> u128 {
+fn encrypt_block_128(plain_text: u128, key: u128) -> (u128, MarlinProof) {
     0
 }
