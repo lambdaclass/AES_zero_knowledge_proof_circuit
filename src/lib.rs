0f9f2a4a6e027d4c411336845352d6f396753d38
Iván Litteri <ilitteri@fi.uba.ar>
2022-12-27 16:07:04 -0300

    Add more linters

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![warn(
     clippy::allow_attributes_without_reason,
     clippy::as_conversions,
+    clippy::as_ptr_cast_mut,
     clippy::unnecessary_cast,
     clippy::clone_on_ref_ptr,
     clippy::create_dir,
@@ -15,15 +16,20 @@
     clippy::float_cmp_const,
     clippy::fn_to_numeric_cast_any,
     clippy::indexing_slicing,
+    clippy::iter_kv_map,
+    clippy::manual_clamp,
+    clippy::manual_filter,
     clippy::map_err_ignore,
+    clippy::uninlined_format_args,
+    clippy::unseparated_literal_suffix,
+    clippy::unused_format_specs,
     clippy::single_char_lifetime_names,
     clippy::str_to_string,
     clippy::string_add,
     clippy::string_slice,
     clippy::string_to_string,
     clippy::todo,
-    clippy::try_err,
-    clippy::unseparated_literal_suffix
+    clippy::try_err
 )]
 #![deny(clippy::unwrap_used, clippy::expect_used)]
 #![allow(
