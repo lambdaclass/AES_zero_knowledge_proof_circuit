788ee4cf474ed8caa0b5d53ad0e7b75e0d26b772
Javier Chatruc <jrchatruc@gmail.com>
2022-12-27 15:47:26 -0300

    Add api skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,77 @@
-use ark_bls12_377::{Bls12_377, Fr, FrParameters, Parameters};
-use ark_ec::bls12::Bls12;
-use ark_ff::Fp256;
-use ark_marlin::{IndexProverKey, IndexVerifierKey, Marlin, Proof, SimpleHashFiatShamirRng};
-use ark_poly::univariate::DensePolynomial;
-use ark_poly_commit::marlin_pc::MarlinKZG10;
+use anyhow::{anyhow, Result};
+use ark_relations::r1cs::{ConstraintSystem, ConstraintSystemRef};
+pub use simpleworks::marlin::generate_rand;
+pub use simpleworks::marlin::serialization::deserialize_proof;
+use simpleworks::{
+    gadgets::ConstraintF,
+    marlin::{MarlinProof, ProvingKey, VerifyingKey},
+};
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub mod aes;
-pub mod ops;
-pub mod prover;
+pub fn encrypt(
+    message: Vec<u8>,
+    secret_key: Vec<u8>,
+    proving_key: ProvingKey,
+) -> Result<(Vec<u8>, MarlinProof)> {
+    let rng = &mut simpleworks::marlin::generate_rand();
+    let constraint_system = ConstraintSystem::<ConstraintF>::new_ref();
 
-pub type MarlinProof =
-    Proof<Fr, MarlinKZG10<Bls12<Parameters>, DensePolynomial<Fp256<FrParameters>>>>;
+    let ciphertext = encrypt_and_generate_constraints(&constraint_system, message, secret_key);
+
+    // Here we clone the constraint system because deep down when generating
+    // the proof the constraint system is consumed and it has to have one
+    // reference for it to be consumed.
+    let cs_clone = (*constraint_system
+        .borrow()
+        .ok_or("Error borrowing")
+        .map_err(|e| anyhow!("{}", e))?)
+    .clone();
+    let cs_ref_clone = ConstraintSystemRef::CS(Rc::new(RefCell::new(cs_clone)));
+
+    let proof = simpleworks::marlin::generate_proof(cs_ref_clone, proving_key, rng)?;
+
+    Ok((ciphertext, proof))
+}
+
+pub fn verify_encryption(verifying_key: VerifyingKey, proof: &MarlinProof) -> Result<bool> {
+    simpleworks::marlin::verify_proof(
+        verifying_key,
+        &[],
+        proof,
+        &mut simpleworks::marlin::generate_rand(),
+    )
+}
+
+pub fn synthetize_keys() -> Result<(ProvingKey, VerifyingKey)> {
+    let rng = &mut simpleworks::marlin::generate_rand();
+    let universal_srs = simpleworks::marlin::generate_universal_srs(rng)?;
+    let constraint_system = ConstraintSystem::<ConstraintF>::new_ref();
+
+    let default_message_input = vec![];
+    let default_secret_key_input = vec![];
+
+    let _ciphertext = encrypt_and_generate_constraints(
+        &constraint_system,
+        default_message_input,
+        default_secret_key_input,
+    );
+
+    simpleworks::marlin::generate_proving_and_verifying_keys(&universal_srs, constraint_system)
+}
+
+fn encrypt_and_generate_constraints(
+    _cs: &ConstraintSystemRef<ConstraintF>,
+    _message: Vec<u8>,
+    _secret_key: Vec<u8>,
+) -> Vec<u8> {
+    /*
+        Here we do the AES encryption, generating the constraints that get all added into
+        `constraint_system`.
+    */
+
+    let ciphertext = vec![];
+    ciphertext
+}
+
+// TODO: Should we add decryption?
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use poc_encryption_proof::{ops, MarlinProof};
+use anyhow::Result;
+use poc_encryption_proof::{encrypt, synthetize_keys, verify_encryption};
 
-fn main() {
-}
+fn main() -> Result<()> {
+    let message = vec![1u8, 2u8, 3u8, 10u8, 9u8, 7u8];
+    let secret_key = vec![1u8, 1u8, 1u8, 1u8];
+    let (proving_key, verifying_key) = synthetize_keys()?;
 
-fn encrypt_block_128(plain_text: u128, key: u128) -> (u128, MarlinProof) {
-    0
+    let (_ciphertext, proof) = encrypt(message, secret_key, proving_key)?;
+
+    assert!(verify_encryption(verifying_key, &proof)?);
+
+    Ok(())
 }
