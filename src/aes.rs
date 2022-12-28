aab2f2cd51ef5c98f815f5a922031dbbfbba40a0
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2022-12-28 10:42:56 -0300

    Matrix gadget helper function (#8)
    * Refactor helpers module
    
    * Add to_matrix_gadget function
    
    * cargo fmt
    
    * cargo clippy

--- src/aes.rs
+++ src/aes.rs
@@ -1,9 +1,9 @@
+use crate::helpers::traits::ToAnyhow;
 use anyhow::Result;
 use ark_ed_on_bls12_381::Fq;
 use ark_r1cs_std::{alloc::AllocVar, uint128::UInt128, R1CSVar, ToBytesGadget};
 use ark_relations::r1cs::ConstraintSystemRef;
 
-use crate::helpers::ToAnyhow;
 // Reference: https://www.gfuzz.de/AES_2.html
 // From what I understand, this is vulnerable to timing attacks,
 // so it is usally done on runtime, but this will do for us for now.
