c46424bff5626eeda0a0ec3c72e92176d671902f
Pablo Deymonnaz <deymonnaz@gmail.com>
2022-12-29 15:57:07 -0300

    ordering

--- src/aes.rs
+++ src/aes.rs
@@ -219,6 +219,70 @@ pub fn mix_columns(input: &[u8; 16]) -> Option<[u8; 16]> {
     Some(ret)
 }
 
+/// This function returns the derived keys from the secret key.
+/// Because AES 128 consists of 11 rounds, the result are 11 128-bit keys,
+/// which we represent as 4 32-bit words, so we compute 44 32-bit elements
+/// W_0, W_1, ..., W_43. The first four constitute the first round key, the
+/// second four the second one, and so on.
+pub fn derive_keys(secret_key: &[u8; 16]) -> Result<[[u8; 16]; 11]> {
+    const ROUND_CONSTANTS: [u32; 10] = [
+        u32::from_be_bytes([0x01, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x02, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x04, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x08, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x10, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x20, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x40, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x80, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x1B, 0x00, 0x00, 0x00]),
+        u32::from_be_bytes([0x36, 0x00, 0x00, 0x00]),
+    ];
+
+    let mut result = [0u32; 44];
+
+    let w_0 = to_u32(&secret_key[..4]);
+    let w_1 = to_u32(&secret_key[4..8]);
+    let w_2 = to_u32(&secret_key[8..12]);
+    let w_3 = to_u32(&secret_key[12..16]);
+
+    result[0] = w_0;
+    result[1] = w_1;
+    result[2] = w_2;
+    result[3] = w_3;
+
+    for i in 4..44 {
+        if i % 4 == 0 {
+            let substituted_and_rotated =
+                to_u32(&crate::substitute_word(&rotate_word(result[i - 1]))?);
+            let w_i = (result[i - 4] ^ (substituted_and_rotated)) ^ ROUND_CONSTANTS[i / 4 - 1];
+            result[i] = w_i;
+        } else {
+            result[i] = result[i - 4] ^ result[i - 1];
+        }
+    }
+
+    let mut ret = [[0_u8; 16]; 11];
+
+    for (i, elem) in result.chunks(4).enumerate() {
+        elem.iter()
+            .map(|e| e.to_be_bytes())
+            .flat_map(|s| s)
+            .collect_slice(&mut ret[i][..]);
+    }
+
+    Ok(ret)
+}
+
+fn to_u32(value: &[u8]) -> u32 {
+    let array_aux: [u8; 4] = [value[0], value[1], value[2], value[3]];
+    u32::from_be_bytes(array_aux)
+}
+
+fn rotate_word(input: u32) -> [u8; 4] {
+    let bytes = input.to_be_bytes();
+    [bytes[1], bytes[2], bytes[3], bytes[0]]
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -292,5 +356,24 @@ mod test {
         let mixed_column_vector = mix_columns(&value_to_mix).unwrap();
 
         assert_eq!(expected_mixed_value, mixed_column_vector);
+    }
+
+    #[test]
+    fn key_expansion() {
+        let secret_key = [
+            0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf,
+            0x4f, 0x3c,
+        ];
+        let result = derive_keys(&secret_key).unwrap();
+
+        assert_eq!(
+            result[10],
+            [
+                0xd0, 0x14, 0xf9, 0xa8, 0xc9, 0xee, 0x25, 0x89, 0xe1, 0x3f, 0x0c, 0xc8, 0xb6, 0x63,
+                0x0c, 0xa6,
+            ]
+        );
+
+        println!("{:x?}", result);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,6 @@ use ark_relations::{
     lc,
     r1cs::{ConstraintSystem, ConstraintSystemRef, LinearCombination},
 };
-use collect_slice::CollectSlice;
 use helpers::traits::ToAnyhow;
 pub use simpleworks::marlin::generate_rand;
 pub use simpleworks::marlin::serialization::deserialize_proof;
@@ -60,19 +59,6 @@ use simpleworks::{
 };
 use std::cell::RefCell;
 use std::rc::Rc;
-
-const ROUND_CONSTANTS: [u32; 10] = [
-    u32::from_be_bytes([0x01, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x02, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x04, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x08, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x10, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x20, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x40, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x80, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x1B, 0x00, 0x00, 0x00]),
-    u32::from_be_bytes([0x36, 0x00, 0x00, 0x00]),
-];
 
 pub fn encrypt(
     message: &[u8],
@@ -136,7 +122,7 @@ fn encrypt_and_generate_constraints(
     */
 
     let mut ciphertext: Vec<u8> = Vec::new();
-    let _round_keys = derive_keys(&secret_key);
+    let _round_keys = aes::derive_keys(&secret_key);
 
     // TODO: Make this in 10 rounds instead of 1.
     // 1 round ECB
@@ -168,56 +154,6 @@ fn encrypt_and_generate_constraints(
     Ok(ciphertext)
 }
 
-/// This function returns the derived keys from the secret key.
-/// Because AES 128 consists of 11 rounds, the result are 11 128-bit keys,
-/// which we represent as 4 32-bit words, so we compute 44 32-bit elements
-/// W_0, W_1, ..., W_43. The first four constitute the first round key, the
-/// second four the second one, and so on.
-fn derive_keys(secret_key: &[u8; 16]) -> Result<[[u8; 16]; 11]> {
-    let mut result = [0u32; 44];
-
-    let w_0 = to_u32(&secret_key[..4]);
-    let w_1 = to_u32(&secret_key[4..8]);
-    let w_2 = to_u32(&secret_key[8..12]);
-    let w_3 = to_u32(&secret_key[12..16]);
-
-    result[0] = w_0;
-    result[1] = w_1;
-    result[2] = w_2;
-    result[3] = w_3;
-
-    for i in 4..44 {
-        if i % 4 == 0 {
-            let substituted_and_rotated = to_u32(&substitute_word(&rotate_word(result[i - 1]))?);
-            let w_i = (result[i - 4] ^ (substituted_and_rotated)) ^ ROUND_CONSTANTS[i / 4 - 1];
-            result[i] = w_i;
-        } else {
-            result[i] = result[i - 4] ^ result[i - 1];
-        }
-    }
-
-    let mut ret = [[0_u8; 16]; 11];
-
-    for (i, elem) in result.chunks(4).enumerate() {
-        elem.iter()
-            .map(|e| e.to_be_bytes())
-            .flat_map(|s| s)
-            .collect_slice(&mut ret[i][..]);
-    }
-
-    Ok(ret)
-}
-
-fn to_u32(value: &[u8]) -> u32 {
-    let array_aux: [u8; 4] = [value[0], value[1], value[2], value[3]];
-    u32::from_be_bytes(array_aux)
-}
-
-fn rotate_word(input: u32) -> [u8; 4] {
-    let bytes = input.to_be_bytes();
-    [bytes[1], bytes[2], bytes[3], bytes[0]]
-}
-
 fn substitute_word(input: &[u8; 4]) -> Result<[u8; 4]> {
     let mut result = [0u8; 4];
     result[0] = substitute_byte(input[0])?;
@@ -226,23 +162,4 @@ fn substitute_word(input: &[u8; 4]) -> Result<[u8; 4]> {
     result[3] = substitute_byte(input[3])?;
 
     Ok(result)
-}
-
-#[test]
-fn key_expansion() {
-    let secret_key = [
-        0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f,
-        0x3c,
-    ];
-    let result = derive_keys(&secret_key).unwrap();
-
-    assert_eq!(
-        result[10],
-        [
-            0xd0, 0x14, 0xf9, 0xa8, 0xc9, 0xee, 0x25, 0x89, 0xe1, 0x3f, 0x0c, 0xc8, 0xb6, 0x63,
-            0x0c, 0xa6,
-        ]
-    );
-
-    println!("{:x?}", result);
 }
