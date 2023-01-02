cc353f22e9631a13845a7912341d3b3437d2b48c
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2023-01-02 10:30:45 -0300

    `AddRoundKey` step circuit version (#18)
    * Refactor functions
    
    * Add 1 round integration test
    
    * cargo clippy
    
    * encrypt_and_generate_constraints first iteration
    
    * Bug fix
    
    * Refactor
    
    * Add round to encrypt_and_generate_constraints
    
    * Refactor
    
    * Add integration test
    
    * cargo fmt
    
    * cargo fmt
    
    * Support variable length plaintexts
    
    * Synthesize keys for variable length plaintext
    
    * Add test for 64-byte plaintext
    
    * cargo fmt
    
    * Make a full aes encryption (10 rounds)
    
    * Update tests
    
    * Fix merge error
    
    * Integrate round keys to the encryption rounds
    
    * Update tests
    
    * cargo fmt
    
    * Update tests
    
    * cargo clippy
    
    * Add round key circuit version
    
    * cargo fmt
    
    * `MixColumns` step circuit version (#19)
    
    * Column mix circuit version
    
    * Test for column mix circuit version
    
    * cargo fmt
    
    * Update mix column fn
    
    Co-authored-by: Pablo Deymonnaz <deymonnaz@gmail.com>

--- src/aes.rs
+++ src/aes.rs
@@ -1,9 +1,9 @@
 use crate::helpers::traits::ToAnyhow;
-use anyhow::{Context, Result};
+use anyhow::{ensure, Context, Result};
 use ark_r1cs_std::{alloc::AllocVar, uint128::UInt128, uint8::UInt8, R1CSVar, ToBytesGadget};
 use ark_relations::r1cs::ConstraintSystemRef;
 use collect_slice::CollectSlice;
-use simpleworks::gadgets::ConstraintF;
+use simpleworks::gadgets::{ConstraintF, UInt8Gadget};
 use std::iter::zip;
 
 // Reference: https://www.gfuzz.de/AES_2.html
@@ -86,6 +86,35 @@ pub fn add_round_key(input_text: &[u8], key: &[u8; 16]) -> [u8; 16] {
         .collect_slice(&mut ret[..]);
 
     ret
+}
+
+pub fn add_round_key_c(
+    input: &[UInt8Gadget],
+    round_key: &[UInt8Gadget],
+) -> Result<Vec<UInt8Gadget>> {
+    ensure!(
+        input.len() == 16,
+        "Input must be 16 bytes length when adding round key"
+    );
+    ensure!(
+        round_key.len() == 16,
+        "Round key must be 16 bytes length when adding round key"
+    );
+
+    let output = input
+        .iter()
+        .zip(round_key)
+        .filter_map(|(input_text_byte, round_key_byte)| {
+            input_text_byte
+                .xor(round_key_byte)
+                .to_anyhow("Error adding round key")
+                .ok()
+        })
+        .collect::<Vec<UInt8Gadget>>();
+
+    ensure!(output.len() == 16, "Error adding round key");
+
+    Ok(output)
 }
 
 pub fn substitute_byte(byte: u8) -> Result<u8> {
@@ -217,6 +246,80 @@ pub fn mix_columns(input: &[u8; 16]) -> Option<[u8; 16]> {
     }
 
     Some(ret)
+}
+
+fn gmix_column_c(input: &[UInt8Gadget; 4]) -> Option<[UInt8Gadget; 4]> {
+    let mut b: Vec<UInt8Gadget> = Vec::new();
+
+    // TODO: Generate constraints for bit shifting.
+    for c in input.iter() {
+        let cs = c.cs();
+
+        let primitive_c = c.value().ok()?;
+        let primitive_h = (primitive_c >> 7_usize) & 1; // arithmetic right shift, thus shifting in either zeros or ones.
+        let primitive_b_byte = (primitive_c << 1_usize) ^ (primitive_h * 0x1B); // implicitly removes high bit because b[c] is an 8-bit char, so we xor by 0x1b and not 0x11b in the next line.
+
+        b.push(UInt8Gadget::new_witness(cs, || Ok(primitive_b_byte)).ok()?);
+    }
+
+    Some([
+        b.first()?
+            .xor(input.get(3)?)
+            .ok()?
+            .xor(input.get(2)?)
+            .ok()?
+            .xor(b.get(1)?)
+            .ok()?
+            .xor(input.get(1)?)
+            .ok()?,
+        b.get(1)?
+            .xor(input.first()?)
+            .ok()?
+            .xor(input.get(3)?)
+            .ok()?
+            .xor(b.get(2)?)
+            .ok()?
+            .xor(input.get(2)?)
+            .ok()?,
+        b.get(2)?
+            .xor(input.get(1)?)
+            .ok()?
+            .xor(input.first()?)
+            .ok()?
+            .xor(b.get(3)?)
+            .ok()?
+            .xor(input.get(3)?)
+            .ok()?,
+        b.get(3)?
+            .xor(input.get(2)?)
+            .ok()?
+            .xor(input.get(1)?)
+            .ok()?
+            .xor(b.first()?)
+            .ok()?
+            .xor(input.first()?)
+            .ok()?,
+    ])
+}
+
+pub fn mix_columns_c(input: &[UInt8Gadget]) -> Option<Vec<UInt8Gadget>> {
+    let mut mixed_input = UInt8Gadget::constant_vec(&[0_u8; 16]);
+    for (i, column) in input.chunks(4).enumerate() {
+        let column_aux = [
+            column.first()?.clone(),
+            column.get(1)?.clone(),
+            column.get(2)?.clone(),
+            column.get(3)?.clone(),
+        ];
+        let column_ret = gmix_column_c(&column_aux)?;
+
+        *mixed_input.get_mut(i * 4)? = column_ret.first()?.clone();
+        *mixed_input.get_mut(i * 4 + 1)? = column_ret.get(1)?.clone();
+        *mixed_input.get_mut(i * 4 + 2)? = column_ret.get(2)?.clone();
+        *mixed_input.get_mut(i * 4 + 3)? = column_ret.get(3)?.clone();
+    }
+
+    Some(mixed_input)
 }
 
 /// This function returns the derived keys from the secret key.
@@ -387,5 +490,63 @@ mod test {
         );
 
         println!("{result:x?}");
+    }
+
+    #[test]
+    fn test_add_round_key_circuit() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let plaintext = UInt8Gadget::new_witness_vec(
+            ark_relations::ns!(cs, "plaintext"),
+            &[
+                0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37,
+                0x07, 0x34,
+            ],
+        )
+        .unwrap();
+        let secret_key = UInt8Gadget::new_witness_vec(
+            ark_relations::ns!(cs, "secret_key"),
+            &[
+                0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf,
+                0x4f, 0x3c,
+            ],
+        )
+        .unwrap();
+        let expected_primitive_result = [
+            0x19, 0x3d, 0xe3, 0xbe, 0xa0, 0xf4, 0xe2, 0x2b, 0x9a, 0xc6, 0x8d, 0x2a, 0xe9, 0xf8,
+            0x48, 0x08,
+        ];
+
+        let after_add_round_key = add_round_key_c(&plaintext, &secret_key).unwrap();
+
+        assert_eq!(
+            after_add_round_key.value().unwrap(),
+            expected_primitive_result
+        );
+        assert!(cs.is_satisfied().unwrap());
+    }
+
+    #[test]
+    fn test_one_round_column_mix_circuit() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let value_to_mix = UInt8Gadget::new_witness_vec(
+            ark_relations::ns!(cs, "value_to_mix"),
+            &[
+                0xd4, 0xbf, 0x5d, 0x30, 0xe0, 0xb4, 0x52, 0xae, 0xb8, 0x41, 0x11, 0xf1, 0x1e, 0x27,
+                0x98, 0xe5,
+            ],
+        )
+        .unwrap();
+        let expected_primitive_mixed_value: [u8; 16] = [
+            0x04, 0x66, 0x81, 0xe5, 0xe0, 0xcb, 0x19, 0x9a, 0x48, 0xf8, 0xd3, 0x7a, 0x28, 0x06,
+            0x26, 0x4c,
+        ];
+
+        let mixed_column_vector = mix_columns_c(&value_to_mix).unwrap();
+
+        assert_eq!(
+            mixed_column_vector.value().unwrap(),
+            expected_primitive_mixed_value
+        );
+        assert!(cs.is_satisfied().unwrap());
     }
 }
