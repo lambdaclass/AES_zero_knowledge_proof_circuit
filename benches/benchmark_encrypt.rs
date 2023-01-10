4f3d2b315c226d2cecb334c0aada67dab414a53f
Iván Litteri <ilitteri@fi.uba.ar>
2023-01-10 18:09:21 -0300

    Implement variable length messages primitive encryption

--- benches/benchmark_encrypt.rs
+++ benches/benchmark_encrypt.rs
@@ -2,11 +2,33 @@ use aes::cipher::KeyInit;
 use anyhow::{ensure, Result};
 use criterion::Criterion;
 
-fn primitive_encrypt(message: &[u8], primitive_secret_key: &aes::Aes128) -> Vec<u8> {
-    let mut encrypted_message = Vec::new();
-    let mut block = digest::generic_array::GenericArray::clone_from_slice(&message);
+fn primitive_encrypy_block(encrypted_message: &mut Vec<u8>, chunk: &[u8], primitive_secret_key: &aes::Aes128) {
+    let mut block = digest::generic_array::GenericArray::clone_from_slice(chunk);
     aes::cipher::BlockEncrypt::encrypt_block(&primitive_secret_key, &mut block);
-    encrypted_message.extend_from_slice(block.as_slice());
+    encrypted_message.extend_from_slice(&block);
+}
+
+fn primitive_encrypt(message: &[u8], primitive_secret_key: &aes::Aes128) -> Vec<u8> {
+    // let mut encrypted_message = Vec::new();
+    // let mut block = digest::generic_array::GenericArray::clone_from_slice(&message);
+    // aes::cipher::BlockEncrypt::encrypt_block(&primitive_secret_key, &mut block);
+    // encrypted_message.extend_from_slice(block.as_slice());
+    // encrypted_message
+    let mut encrypted_message: Vec<u8> = Vec::new();
+
+    message.chunks_exact(16).for_each(|chunk| {
+        primitive_encrypy_block(&mut encrypted_message, chunk, primitive_secret_key);
+    });
+
+    let mut extended_chunk = [0_u8; 16];
+    for (extended_chunk_byte, chunk_byte) in extended_chunk
+        .iter_mut()
+        .zip(message.chunks_exact(16).remainder())
+    {
+        *extended_chunk_byte = *chunk_byte;
+    }
+    primitive_encrypy_block(&mut encrypted_message, &extended_chunk, primitive_secret_key);
+
     encrypted_message
 }
 
@@ -37,6 +59,7 @@ pub fn encrypt_message_with_bytes(c: &mut Criterion, amount_of_bytes: usize) -> 
     let ciphertext = primitive_encrypt(&message, &primitive_secret_key);
 
     let mut group = c.benchmark_group("Encryption");
+    group.sample_size(10);
     group.bench_function(format!("{amount_of_bytes}_message_encryption"), |b| {
         b.iter(|| {
             poc_encryption_proof::encrypt(&message, &key, &ciphertext, proving_key.clone())
