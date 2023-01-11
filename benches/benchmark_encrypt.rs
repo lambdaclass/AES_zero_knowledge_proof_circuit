1bd6659d9d76850159787524483c9a094d65b1d6
Iván Litteri <ilitteri@fi.uba.ar>
2023-01-11 10:49:00 -0300

    Restore default sample_size

--- benches/benchmark_encrypt.rs
+++ benches/benchmark_encrypt.rs
@@ -42,7 +42,7 @@ pub fn encrypt_message_with_bytes(c: &mut Criterion, amount_of_bytes: usize) -> 
     let ciphertext = primitive_encrypt(&message, &primitive_secret_key);
 
     let mut group = c.benchmark_group("Encryption");
-    group.sample_size(10);
+    group.sample_size(100);
     group.bench_function(format!("{amount_of_bytes}_message_encryption"), |b| {
         b.iter(|| {
             poc_encryption_proof::encrypt(&message, &key, &ciphertext, proving_key.clone())
