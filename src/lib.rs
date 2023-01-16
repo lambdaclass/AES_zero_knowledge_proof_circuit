9fcc711e33263a335de43ccf804c07cc30cd4532
Iván Litteri <ilitteri@fi.uba.ar>
2023-01-16 11:22:46 -0300

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -148,8 +148,7 @@ fn byte_to_field_array(byte: u8) -> Vec<ConstraintF> {
 pub fn synthesize_keys(plaintext_length: usize) -> Result<(ProvingKey, VerifyingKey)> {
     let rng = &mut simpleworks::marlin::generate_rand();
     // This parameters support encrypting messages up to 1kb length.
-    let universal_srs =
-        simpleworks::marlin::generate_universal_srs(866_944, 513, 4_062_064, rng)?;
+    let universal_srs = simpleworks::marlin::generate_universal_srs(866_944, 513, 4_062_064, rng)?;
     let constraint_system = ConstraintSystem::<ConstraintF>::new_ref();
 
     let default_message_input = vec![0_u8; plaintext_length];
